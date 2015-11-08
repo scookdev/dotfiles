419fc7b6b9c168b08c81544ebb475b251a03ca90
m8ss <mace2345@gmail.com>
2015-11-08 11:03:14 -0500

    mend

---
+++ .install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+for f in *; do
+  BASE=`basename $f`;
+  CWD=`pwd`
+
+  # Don't link install script or dotfiles
+  if [[ ("${BASE:0:1}" == ".")]]; then
+    continue
+  fi
+
+  # Don't link install the readme
+  if [[ "${BASE}" == "README.md" ]]; then
+    continue
+  fi
+
+  # Don't link install anything in other directory
+  if [[ "${BASE}" == "other" ]]; then
+      continue
+  fi
+
+  ln -sin "${CWD}/${BASE}" "${HOME}/.${BASE}"
+done
---
+++ README.md
@@ -0,0 +1,16 @@
+# Dotfiles
+
+A collection of my dotfiles. Running the installer will prompt you Y/N to
+overwrite symlinks (`ln -sin`) for every file in this directory to your
+home directory.
+
+
+### Installation
+
+```sh
+git clone --recursive https://github.com/m8ss/dotfiles.git ~/.dotfiles
+
+cd ~/.dotfiles
+
+./.install.sh
+```
